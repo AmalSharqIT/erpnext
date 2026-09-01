0e6c5b212a8488e8542526faa5d95b80ce2c5ce8
elshafei-developer <el.shafei.developer@gmail.com>
2026-09-01 08:47:08 +0000

    fix: get only item for select supplier

--- erpnext/stock/doctype/material_request/material_request.py
+++ erpnext/stock/doctype/material_request/material_request.py
@@ -568,10 +568,10 @@ def make_purchase_order(source_name, target_doc=None, args=None):
 	requested_qty = args.get("requested_qty") or {}
 
 	def postprocess(source, target_doc):
-		if frappe.flags.args and frappe.flags.args.default_supplier:
+		if target_doc.get("doctype") == "Purchase Order" and target_doc.get("supplier"):
 			supplier_items = []
 			for d in target_doc.items:
-				if frappe.flags.args.default_supplier == d.get("supplier"):
+				if target_doc.supplier == d.get("supplier"):
 					supplier_items.append(d)
 			target_doc.items = supplier_items
 		target_doc.is_subcontracted = is_subcontracted
